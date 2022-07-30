835610469bd800cad32946d42fcd5afb3c5579f4
dev-flare <dev.flare@pm.me>
2022-07-31 00:36:09 +0900

    feat (style) tailwindcss, sass 설치 및 적용 등
    - tailwindcss 관련 의존성 설치
    - scss modules 활용을 위한 sass 설치 및 Button style 테스트

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.{tsx,jsx}', './pages/**/*.{tsx,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
